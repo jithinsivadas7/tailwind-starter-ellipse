60c7115563d5699b92dda664e8402a657c45cba5
Jithin S <jithin@dieselhaus.com>
2022-01-06 00:01:39 +0530

    Tailwind css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,29 @@
-
-   
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: {
     enabled: false,
-   },
-   content:  [
+  },
+  content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: [ ...defaultTheme.fontFamily.sans],
+        sans: [...defaultTheme.fontFamily.sans],
       },
-      colors:{
+      colors: {
         brown: {
-        50: '#fbf9f7',
-        100: '#f9f8f5',
-        200: '#f7f4f0',
-        300: '#f4f1eb',
-        400: '#f2eee7',
-      }
+          50: '#fbf9f7',
+          100: '#f9f8f5',
+          200: '#f7f4f0',
+          300: '#f4f1eb',
+          400: '#f2eee7',
+        },
+        'black-gray': '#1f1f1f',
+      },
     },
-    },
-    
+
   },
   plugins: [],
 }
